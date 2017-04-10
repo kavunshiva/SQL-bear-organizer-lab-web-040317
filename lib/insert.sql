e6c640f6b480fc2e8b487c49e0df7210209609f6
kavunshiva <kavunshiva@gmail.com>
2017-04-10 16:32:29 -0400

    get bears to cooperate (and not kill us)

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+-- name TEXT,
+-- age INTEGER,
+-- gender CHAR(1),
+-- color TEXT,
+-- temperament TEXT,
+-- alive INTEGER
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 23, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 25, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 45, NULL, NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, NULL, NULL, NULL, NULL);
